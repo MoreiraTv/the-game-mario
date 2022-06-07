8595ea767b67f34342f75a31581f2ae2743f0381
Matheus Moreira <matheusmoreira08@gmail.com>
2022-06-07 00:49:32 -0300

    adc score and reset game

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
+const score = document.querySelector('.score p label');
+const gameOver = document.querySelector('.game-over');
 
 const jump = () => {
   mario.classList.add('jump');
@@ -8,12 +10,32 @@ const jump = () => {
   }, 500)
 }
 
-const loop = setInterval(() => {
+
+//Score do jogo
+let scorePoint = 0;
+const pointsLoop = setInterval(() => {
+  const pipePostition = pipe.offsetLeft;
+  if (pipePostition) {
+    scorePoint++;
+    score.innerHTML = scorePoint
+    lastPipePosition = pipePostition;
+  }
+  if(window.getComputedStyle(gameOver).display != 'none'){
+    clearInterval(pointsLoop);
+  }
+  
+  console.log(scorePoint);
+
+
+}, 100)
+
+
+//verify jump
+const verifyPipe = () =>{
   const pipePostition = pipe.offsetLeft;
   const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
 
-  console.log(marioPosition)
-
+  // console.log(pipePostition)
   if (pipePostition <= 120 && pipePostition > 0 && marioPosition < 80) {
 
     pipe.style.animation = 'none';
@@ -21,7 +43,8 @@ const loop = setInterval(() => {
 
     mario.style.animation = 'none';
     mario.style.bottom = `${marioPosition}px`
-
+    
+    gameOver.style.display = 'inherit';
     mario.src = './images/game-over.png'
     mario.style.width = '75px';
     mario.style.marginLeft = '50px'
@@ -29,6 +52,27 @@ const loop = setInterval(() => {
 
     clearInterval(loop);
   }
-}, 10)
+}
+
+let loop = setInterval(verifyPipe, 10)
+
+const reset = () => {
+  if(window.getComputedStyle(gameOver).display != 'none'){
+    pipe.style.animation = 'pipe-animation 1.5s infinite linear';
+    pipe.style.left = ``;
+    pipe.style.right = `-80px`;
+  
+    mario.style = '';
+    // mario.style.bottom = `0px`
+    
+    gameOver.style.display = 'none';
+    mario.src = './images/mario.gif'
+    mario.style.width = '150px';
+    mario.style.marginLeft = '0'
+    loop = setInterval(verifyPipe, 10)
+  }  
+}
+
 
 document.addEventListener('keydown', jump);
+document.addEventListener('keydown', reset);
